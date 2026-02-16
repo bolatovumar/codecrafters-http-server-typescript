2e79818bfc00a5f1374508262467114a2eb2d85f
Umar Bolatov <bolatovumar@gmail.com>
2026-02-15 16:30:05 -0800

    refactor: implement request handling for HTTP server

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,18 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.end();
+  socket.on("data", (buf) => {
+    const req = buf.toString("utf8");
+    const [_, path] = req.split(" ");
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n");
+    }
+
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
