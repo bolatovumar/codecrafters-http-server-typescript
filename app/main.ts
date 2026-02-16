8ee074d48a937020d020d2468b3ae297166a5d5f
Umar Bolatov <bolatovumar@gmail.com>
2026-02-15 16:38:06 -0800

    feat: add echo functionality to HTTP server

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,9 @@ const server = net.createServer((socket) => {
     const [_, path] = req.split(" ");
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.split('/')[1] === 'echo') {
+      const echoText = path.split('/')[2];
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoText.length}\r\n\r\n${echoText}`);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
