a6a1d2c1f199a535523353dd4fba497a1afcc205
Umar Bolatov <bolatovumar@gmail.com>
2026-02-15 17:07:17 -0800

    feat: enhance request handling to support user-agent response

--- app/main.ts
+++ app/main.ts
@@ -6,12 +6,23 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (buf) => {
     const req = buf.toString("utf8");
-    const [_, path] = req.split(" ");
+    const [reqLine, ...rest] = req.split("\r\n");
+    // console.log(`Request Line: ${reqLine}`);
+    const body = rest.pop();
+    const headersMap = new Map<string, string>();
+    for (const header of rest) {
+      const [key, value] = header.split(": ");
+      headersMap.set(key, value);
+    }
+    const [_, path] = reqLine.split(" ");
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (path.split('/')[1] === 'echo') {
       const echoText = path.split('/')[2];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoText.length}\r\n\r\n${echoText}`);
+    } else if (path.split("/")[1] === 'user-agent') {
+      const userAgent = headersMap.get("User-Agent") || "";
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
